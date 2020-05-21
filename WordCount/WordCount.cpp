0ac2c014b3e5cfb92469ac161e7ad09fbc075342
Zhang GengYuan <873918442@qq.com>
2020-05-21 13:23:07 +0800

    添加: 代码已测试并上传

---
+++ WordCount/WordCount.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    int i = 0 ,j = 0;
+    char c;
+    FILE* file;
+    errno_t err; 
+    err = fopen_s(&file, argv[2] , "r");  
+        if (err != 0)
+        {
+            printf("文件打开失败！\n");
+            return 0;
+        }
+        else
+        {
+        if (strcmp(argv[1], "-c") == 0)     //字符数
+        {
+        
+            while ((c = fgetc(file)) != EOF)
+            {
+                i++;
+            }
+         printf("字符数为:%d\n", i);
+        }
+
+        if (strcmp(argv[1], "-w") == 0)     //字符数
+        {
+            while ((c = fgetc(file)) != EOF)
+            {
+                if (c == ' ' || c == ',')
+                {
+                    j++;
+                }
+            }
+        printf("单词数为:%d\n", j+1);
+        }
+        }
+        fclose(file);
+    return 0;
+}
